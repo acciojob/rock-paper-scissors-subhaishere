353b246ae06c6571cdf95ea95cca62850a12c142
subhaishere <60627114+subhaishere@users.noreply.github.com>
2023-08-14 11:25:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,113 +1,92 @@
-var gameNumberEl = document.getElementById("game-number");
-  var gameContainerEl = document.getElementById("game-container");
-  var rockBtnEl = document.getElementById("rock");
-  var paperBtnEl = document.getElementById("paper");
-  var scissorBtnEl = document.getElementById("scissors");
-  var computerChooseEl = document.getElementById("computer-choose");
-  var roundResultEl = document.getElementById("round-result");
-  var userPointsEl = document.getElementById("user-points");
-  var computerPointsEl = document.getElementById("computer-points");
-  var roundsLeftEl = document.getElementById("rounds-left");
-  var gameResultEl = document.getElementById("game-result");
- 
+const gameNumberInput = document.getElementById("game-number");
+const playButton = document.getElementById("play-game");
+const choices = document.querySelectorAll(".choice");
+const roundResult = document.querySelector("[data-ns-test='round-result']");
+const roundsLeft = document.querySelector("[data-ns-test='rounds-left']");
+const userPoints = document.querySelector("[data-ns-test='user-points']");
+const computerPoints = document.querySelector("[data-ns-test='computer-points']");
+const gameResult = document.querySelector("[data-ns-test='game-result']");
 
-  const ROCK = 0;
-  const PAPER = 1;
-  const SCISSORS = 2;
- 
+// Define the computer's choices
+const computerChoices = ["ROCK", "PAPER", "SCISSORS"];
 
-  function playGame() {
-  userPoints = 0;
-  userPointsEl.innerText = 0;
-  computerPoints = 0;
-  computerPointsEl.innerText = 0;
- 
+// Initialize game variables
+let turnsLeft, userScore, computerScore;
 
-  roundsLeft = parseInt(gameNumberEl.value, 10);
-  if (roundsLeft < 0) {
-  alert("Number of turns cannot be -ve");
+// Event listener for the play button
+playButton.addEventListener("click", startGame);
+
+// Event listeners for the user choices
+choices.forEach(choice => {
+  choice.addEventListener("click", playRound);
+});
+
+// Start the game
+function startGame() {
+  turnsLeft = parseInt(gameNumberInput.value);
+  userScore = 0;
+  computerScore = 0;
+
+  roundsLeft.textContent = turnsLeft;
+  userPoints.textContent = userScore;
+  computerPoints.textContent = computerScore;
+  gameResult.textContent = "";
+
+  enableChoices();
+}
+
+// Enable user choices
+function enableChoices() {
+  choices.forEach(choice => {
+    choice.disabled = false;
+  });
+}
+
+// Disable user choices
+function disableChoices() {
+  choices.forEach(choice => {
+    choice.disabled = true;
+  });
+}
+
+// Play a round of the game
+function playRound(event) {
+  const userChoice = event.target.getAttribute("data-ns-test");
+  const computerChoice = computerChoices[Math.floor(Math.random() * 3)];
+
+  roundResult.textContent = "";
+
+  if (userChoice === computerChoice) {
+    roundResult.textContent = "TIE";
+  } else if (
+    (userChoice === "rock" && computerChoice === "SCISSORS") ||
+    (userChoice === "paper" && computerChoice === "ROCK") ||
+    (userChoice === "scissors" && computerChoice === "PAPER")
+  ) {
+    roundResult.textContent = "WON";
+    userScore++;
   } else {
-  roundsLeftEl.innerText = roundsLeft;
-  gameContainerEl.style.display = "block";
- 
-
-  gameResultEl.style.display = "none";
+    roundResult.textContent = "LOSE";
+    computerScore++;
   }
+
+  turnsLeft--;
+  roundsLeft.textContent = turnsLeft;
+  userPoints.textContent = userScore;
+  computerPoints.textContent = computerScore;
+
+  if (turnsLeft === 0) {
+    disableChoices();
+    endGame();
   }
- 
+}
 
-  // game variables
-  var userPoints = 0;
-  var computerPoints = 0;
-  var roundsLeft = parseInt(gameNumberEl.value, 10);
-  roundsLeftEl.innerText = roundsLeft;
- 
+// End the game and display the result
+function endGame() {
+  if (userScore === computerScore) {
+    gameResult.textContent}
 
- 
 
-  function userChoose(e) {
- 
-
-  if (roundsLeft <= 0) return;
- 
-
-  const choices = ["ROCK", "PAPER", "SCISSORS"];
-  const outcomes = ["WON", "TIE", "LOSE"];
-  const userChoose = parseInt(e.target.value, 10);
-  const computerChoose = Math.floor(Math.random() * 3);
-  window.computerChoose = computerChoose
-  console.log(window.computerChoose);
-  computerChooseEl.innerText = choices[computerChoose];
-  var outcome = 1; // [win, tie, loose]
- 
-
-  if ((choices[computerChoose] == 'ROCK' && choices[userChoose] == 'PAPER') || (choices[computerChoose] == 'SCISSORS' && choices[userChoose] == 'ROCK') || (choices[computerChoose] == 'PAPER' && choices[userChoose] == 'SCISSORS')) {
-  // use Wins
-  outcome = 0;
-  userPoints += 1;
-  } else if ((choices[computerChoose] == 'PAPER' && choices[userChoose] == 'ROCK') || (choices[computerChoose] == 'ROCK' && choices[userChoose] == 'SCISSORS') || (choices[computerChoose] == 'SCISSORS' && choices[userChoose] == 'PAPER')) {
-  // lose
-  outcome = 2;
-  computerPoints += 1;
-  } else {
-  // tie
-  outcome = 1;
-  }
- 
-
-  // setting game variables after move
-  roundsLeft -= 1;
- 
-
- 
-
-  roundResultEl.innerText = `${outcomes[outcome]}`;
-  roundResultEl.style.display = "block";
- 
-
-  userPointsEl.innerText = userPoints;
-  computerPointsEl.innerText = computerPoints;
- 
-
-  roundsLeftEl.innerText = roundsLeft;
- 
-
-  if (roundsLeft == 0) {
-  // game over
-  gameResultEl.innerText = `${outcomes[userPoints == computerPoints ? 1 : userPoints > computerPoints ? 0 : 2]}`;
-  gameResultEl.style.display = "block";
- 
-
-  gameContainerEl.style.display = "none";
- 
-
-  }
- 
-
-  }
- 
-
-  // button click
-  rockBtnEl.addEventListener('click', userChoose);
-  paperBtnEl.addEventListener('click', userChoose);
-  scissorBtnEl.addEventListener('click', userChoose)
+}
+	
+   
